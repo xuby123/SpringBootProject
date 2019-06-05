12c50d0df0286585cd8808fafc67442acc07271a
xuby <1090300742@qq.com>
2019-06-05 21:58:55 +0800

    init

--- springboot-demo/src/main/java/com/joe/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/joe/SpringbootDemoApplication.java
@@ -1,8 +1,18 @@
-package com.joe.springbootdemo;
+package com.joe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *<p>功能描述: SpringBoot入口</p >
+ *<ul>
+ *<li>@param null </li>
+ *<li>@return </li>
+ *<li>@throws </li>
+ *<li>@author xubiyu</li>
+ *<li>@date 2019-06-05 21:46</li>
+ *</ul>
+*/
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
